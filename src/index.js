7dba6873bdf76ffc9ae9879898bd3e97474672b6
Nyia <lornyia@gmail.com>
2022-12-06 22:33:36 -0600

    install react icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//connect to public/index.html
+import App from './App';
+
 ReactDom.render(<App/>, document.getElementById('root'));
