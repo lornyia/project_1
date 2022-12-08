9838912bebe2d380c02794b7ed0fff9c954cab95
Nyia <lornyia@gmail.com>
2022-12-07 21:12:48 -0600

    add tags

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className='App'>
+      <div className='gradient__bg' >
+        <Navbar  />
+        <Header />
+      </div>
+      <Brand />
+      <WhatLor />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
